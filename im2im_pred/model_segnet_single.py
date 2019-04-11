ed26bcca150dfef38d14c11bff4191b7d8ca107f
Shikun Liu <sk.lorenmt@gmail.com>
2019-04-11 15:14:33 +0800

    fix typo

--- im2im_pred/model_segnet_single.py
+++ im2im_pred/model_segnet_single.py
@@ -284,13 +284,13 @@ for epoch in range(total_epoch):
 
             avg_cost[index, 12:] += cost[12:] / test_batch
 
-    if opt.type == 'semantic':
+    if opt.task == 'semantic':
         print('Epoch: {:04d} | TRAIN: {:.4f} {:.4f} {:.4f} TEST: {:.4f} {:.4f} {:.4f}'
           .format(index, avg_cost[index, 0], avg_cost[index, 1], avg_cost[index, 2], avg_cost[index, 12], avg_cost[index, 13], avg_cost[index, 14]))
-    if opt.type == 'depth':
+    if opt.task == 'depth':
         print('Epoch: {:04d} | TRAIN: {:.4f} {:.4f} {:.4f} TEST: {:.4f} {:.4f} {:.4f}'
           .format(index, avg_cost[index, 3], avg_cost[index, 4], avg_cost[index, 5], avg_cost[index, 15], avg_cost[index, 16], avg_cost[index, 17]))
-    if opt.type == 'semantic':
+    if opt.task == 'semantic':
         print('Epoch: {:04d} | TRAIN: {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} TEST: {:.4f} {:.4f} {:.4f} {:.4f} {:.4f} {:.4f}'
           .format(index, avg_cost[index, 6], avg_cost[index, 7], avg_cost[index, 8], avg_cost[index, 9], avg_cost[index, 10], avg_cost[index, 11],
                   avg_cost[index, 18], avg_cost[index, 19], avg_cost[index, 20], avg_cost[index, 21], avg_cost[index, 22], avg_cost[index, 23]))
