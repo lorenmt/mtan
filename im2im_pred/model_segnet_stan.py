bdac5321a87102e6ea646ab7b7d73d7daa85e968
Shikun Liu <sk.lorenmt@gmail.com>
2019-04-11 15:44:05 +0800

    fix index

--- im2im_pred/model_segnet_stan.py
+++ im2im_pred/model_segnet_stan.py
@@ -166,11 +166,11 @@ class SegNet(nn.Module):
 
         # define task prediction layers
         if opt.task == 'semantic':
-            pred = F.log_softmax(self.pred_task(g_decoder[i][1]), dim=1)
+            pred = F.log_softmax(self.pred_task(atten_decoder[0][-1][-1]), dim=1)
         if opt.task == 'depth':
-            pred = self.pred_task(g_decoder[i][1])
+            pred = self.pred_task(atten_decoder[0][-1][-1])
         if opt.task == 'normal':
-            pred = self.pred_task(g_decoder[i][1])
+            pred = self.pred_task(atten_decoder[0][-1][-1])
             pred = pred / torch.norm(pred, p=2, dim=1, keepdim=True)
         return pred
 
